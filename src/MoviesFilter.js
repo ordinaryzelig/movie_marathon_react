5ca87dbc59614d4f63c20028fdbc0c7d1903fec7
Jared Ning <jared@redningja.com>
2016-11-26 21:16:14 -0600

    Refactor filter slider to make component do most work.

--- src/MoviesFilter.js
+++ src/MoviesFilter.js
@@ -7,33 +7,21 @@ class MoviesFilter extends Component {
   constructor(props) {
     super(props);
 
-    this.sortedShowtimes = []
+    this.datetimes = []
     for (var movie of props.movies) {
-      this.sortedShowtimes = this.sortedShowtimes.concat(movie.showtimes);
+      this.datetimes = this.datetimes.concat(movie.showtimes.map(function(showtime) {return showtime.datetime}));
     }
-    this.sortedShowtimes = this.sortedShowtimes.sort(function(a, b) {
-      if (a.datetime < b.datetime) { return -1; }
-      if (a.datetime > b.datetime) { return 1; }
-      return 0;
-    });
   }
 
   render() {
     return(
       <div>
         <MoviesFilterCheckboxes movies={this.props.movies} movieChecked={this.props.movieChecked} />
-        <MoviesFilterTimes earliestTime={this.earliestTime(this.props.movies)} latestTime={this.latestTime(this.props.movies)} />
+        <MoviesFilterTimes datetimes={this.datetimes} />
       </div>
     );
   }
 
-  earliestTime(movies) {
-    return this.sortedShowtimes[0].datetime;
-  }
-
-  latestTime(movies) {
-    return this.sortedShowtimes[this.sortedShowtimes.length - 1].datetime;
-  }
 }
 
 export default MoviesFilter;
--- src/MoviesFilterTimes.js
+++ src/MoviesFilterTimes.js
@@ -6,27 +6,53 @@ import Formatter from './formatter';
 class MoviesFilterTimes extends Component {
   constructor(props) {
     super(props);
-    this.formatTip = this.formatTip.bind(this);
-    this.earliestTime = this.addMinutes(this.props.earliestTime, -this.props.earliestTime.getMinutes());
-    this.latestTime = this.addMinutes(this.props.latestTime, 60 - this.props.latestTime.getMinutes());
+
+    this.sortedShowtimes = this.props.datetimes.sort();
+
+    this.earliestTime = this.props.datetimes[0]
+    this.floorTime = this.floor(this.earliestTime);
+
+    this.latestTime = this.props.datetimes[this.props.datetimes.length - 1];
+    this.ceilingTime = this.ceiling(this.latestTime);
+
     this.max = (this.latestTime - this.earliestTime) / 60000;
+
+    this.formatTip = this.formatTip.bind(this);
   }
 
   render() {
     return(
-      <div>
-        <Rcslider min={0} max={this.max} step={15} tipFormatter={this.formatTip}/>
-      </div>
+      <Rcslider
+        min={0}
+        max={this.max}
+        step={15}
+        tipFormatter={this.formatTip}
+        range
+        defaultValue={[0, this.max]}
+        onChange={this.onChange}
+      />
     );
   }
 
+  onChange(value) {
+    console.log(value);
+  }
+
   formatTip(minutes) {
-    var adjusted = this.addMinutes(this.props.earliestTime, minutes);
+    var adjusted = this.addMinutes(this.earliestTime, minutes);
     return Formatter.formatTime(adjusted);
   }
 
-  addMinutes(date, minutes) {
-    return new Date(date.getTime() + minutes*60000);
+  addMinutes(datetime, minutes) {
+    return new Date(datetime.getTime() + minutes*60000);
+  }
+
+  floor(datetime) {
+    return this.addMinutes(datetime, -datetime.getMinutes());
+  }
+
+  ceiling(datetime) {
+    return this.addMinutes(datetime, 60 - datetime.getMinutes());
   }
 }
 
